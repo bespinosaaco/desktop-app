823b54b8da9f3e440ff860eeca0e678a30ca808c
Brian Espinosa-Acosta <brianespinosaacosta@gmail.com>
2024-12-09 15:46:09 -0330

    Initial Commit0.1

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # The app begins here!
-st.logo('logo1.png', size="large", link=None, icon_image=None)
+st.logo('Logo1.jpg', size="large", link=None, icon_image=None)
 st.title('Desktop App')
 st.caption('Made by Brian')
 
